edd627474d1ff3599b1a72d156ce4b50cf832aaf
yurian51 <yurianarshad@gmail.com>
2026-08-18 11:03:48 +0300

    feat(api): add global configuration and health module

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { HealthModule } from './health/health.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, cache: true }),
+    HealthModule,
+  ],
+})
+export class AppModule {}
